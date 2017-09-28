af098dab9f0165420ec814e5e8a8b376140a0c8d
akash09766 <wangalwar.akash@gmail.com>
2017-09-28 15:59:43 +0530

    bug fixes

--- app/src/main/java/com/skylightdeveloper/moviemaasala/adapter/PopularMovieAdapter.java
+++ app/src/main/java/com/skylightdeveloper/moviemaasala/adapter/PopularMovieAdapter.java
@@ -105,6 +105,7 @@ public class PopularMovieAdapter extends RecyclerView.Adapter<PopularMovieAdapte
                 listener.onMovieItemClick(item, (View) holder.mMovieIv);
             }
         });
+
     }
 
 
--- app/src/main/java/com/skylightdeveloper/moviemaasala/config/MConstants.java
+++ app/src/main/java/com/skylightdeveloper/moviemaasala/config/MConstants.java
@@ -12,10 +12,11 @@ public class MConstants {
     public static final String IMAGE_URL_BASE_URL_TYPE_ORIGINAL = "https://image.tmdb.org/t/p/w185/";
     public static final String IMAGE_URL_BASE_URL_TYPE_HEADER = "https://image.tmdb.org/t/p/w780/";
 
-//    public static final String API_KEY = "enter your themoviedb API here ";// TODO : enter your themoviedb API here
+    public static final String API_KEY = "enter your themoviedb API here ";// TODO : enter your themoviedb API here
 
     public static final String POLULAR_MOVIES = "movie/popular";
     public static final String LANGAUGE = "en-US";
     public static final String MOVIE_DETAILS_PARCELABLE_FILTER = "moview_details_filter";
+
 
 }
